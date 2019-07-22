1254811ffb8594ff515e9596ecc682ad2892f053
rfrieger <rfrieger@gmail.com>
2019-07-22 10:48:57 -0400

    before merge form clear

--- angularclient/src/app/post-form/post-form.component.ts
+++ angularclient/src/app/post-form/post-form.component.ts
@@ -28,10 +28,14 @@ export class PostFormComponent {
     this.post.author = this.loggedInUser.name
     this.post.user_id = this.loggedInUser.user_id
     // this.post.user_id = this.loggedInUser_Id;
-    this.showNewPost = false;
+    this.closePostModal
     await this.postService.save(this.post).then( ()=> console.log("success"));
   
     this.sendEmit()
+  }
+
+  closePostModal() {
+    this.showNewPost = false
   }
 
   sendEmit() {
