f029db809de7e6a36930f0e6bd42cc67c2fb2d1f
alilinykh <aililnykh@dtcc.edu>
2019-07-23 15:41:04 -0400

    commit 3 pm tuesday

--- angularclient/src/app/post-form/post-form.component.ts
+++ angularclient/src/app/post-form/post-form.component.ts
@@ -4,6 +4,9 @@ import { PostService } from '../service/post-service';
 import { Post } from '../post';
 import { User } from '../user';
 
+import {FormsModule} from '@angular/forms';
+
+
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
